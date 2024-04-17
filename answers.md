873fc4416c2fb1213d1f852f11b03d73c5fd3eed
joshwa829 <143100721+joshwa829@users.noreply.github.com>
2024-04-17 22:14:26 +0000

    Lab 07

--- answers.md
+++ answers.md
@@ -1,6 +1,6 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
+**Name:**___Joshua Allison______________________
 **Name:**_________________________
 
 
@@ -8,10 +8,10 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 
-- **2)**
+- **2)** The work of reachable is O(n+m)
 
-- **4)**
+- **4)** The reachable function is only called once since the graph is undirected. So the worst case is only once 
 
-- **5)**
+- **5)** The work of connected is O(n+m)
 
-- **7)**
+- **7)**  If we switched the graph to an adjacency matrix, the work would still be O(n+m) and would not change the overall work.
--- main.py
+++ main.py
@@ -17,8 +17,12 @@ def reachable(graph, start_node):
     result = set([start_node])
     frontier = set([start_node])
     while len(frontier) != 0:
-        ###TODO
-        pass
+        curr_node = frontier.pop()
+        near_nodes = graph[curr_node]
+        for node in near_nodes:
+            if node not in result:
+                result.add(node)
+                frontier.add(node)
     return result
 
 
@@ -26,8 +30,10 @@ def reachable(graph, start_node):
 
 
 def connected(graph):
-    ### TODO
-    pass
+    prev_nodes = reachable(graph, list(graph.keys())[0])
+    if len(graph) == len(prev_nodes):
+      return True
+    return False
 
 
 
@@ -37,6 +43,13 @@ def n_components(graph):
     Returns:
       the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    
+    prev_nodes = set()  
+    count = 0
+    for node in graph:
+      if node not in prev_nodes:
+        current_visited = reachable(graph, node)
+        prev_nodes.update(current_visited)
+        count += 1  
+    return count
 
